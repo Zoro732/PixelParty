3ac4a1ff4d200d607719e784bb7d3cc962cbbe71
Amaury <amaury.gielen@edu.esiee.fr>
2024-10-25 17:56:22 +0200

    1st SETTINGS VERSION

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,82 +1,80 @@
 package com.example.helloworld;
 
-import static android.app.PendingIntent.getActivity;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
-    private GameView gameView;
-    private Button changeSpriteButton;
+    private ImageButton BackButton;
     private Vibrator vibrator;
-    long[] timings = {0, 100, 330, 100};  // Vibrer 200ms, pause 100ms, vibrer 300ms
-    int[] amplitudes = {0, 100, 0, 255};  // Intensité correspondante (0 pour pas de vibration, 100 et 255 pour vibrer)
+    private boolean hasVibrated = false; // Booléen pour vérifier si la vibration a déjà eu lieu
+    long[] timings = {0, 100};  // Vibre pendant 100ms, puis s'arrête
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        hideNavigationBar();
         setContentView(R.layout.activity_main);
-        // Get instance of Vibrator from current Context
+
+        // Obtenir l'instance de Vibrator à partir du contexte actuel
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        // Créer une instance de GameView et l'ajouter au FrameLayout
-        FrameLayout gameFrame = findViewById(R.id.gameFrame);
-        gameView = new GameView(this);
-        gameFrame.addView(gameView);
-
         // Associer le bouton
-        changeSpriteButton = findViewById(R.id.changeSpriteButton);
-        //VibrationEffect vibrationEffect = VibrationEffect.createWaveform(timings, amplitudes, -1);
+        BackButton = findViewById(R.id.back);
 
-        // Définir l'écouteur d'événements pour changer le spritesheet
-        changeSpriteButton.setOnTouchListener(new View.OnTouchListener() {
+        // Définir l'écouteur d'événements pour le bouton
+        BackButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Changer le spritesheet quand le bouton est pressé
-                    gameView.changeSpriteSheet(R.drawable.attack);  // Remplacer par ton nouveau spritesheet
-                    // Vibrate for 300 milliseconds
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        vibrator.vibrate(timings,0);
+                    // Vérifiez si la vibration n'a pas déjà eu lieu
+                    if (!hasVibrated) {
+                        // Vibration
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            vibrator.vibrate(timings, -1); // Utiliser -1 pour une vibration unique
+                        }
+                        hasVibrated = true; // Marquez que la vibration a eu lieu
                     }
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    // Restaurer le spritesheet original quand le bouton est relâché
-                    gameView.changeSpriteSheet(R.drawable.idle);  // Remplacer par le spritesheet original
-                    vibrator.cancel();
+                    // Réinitialiser le booléen lorsque le bouton est relâché
+                    hasVibrated = false;
                 }
                 return true;
             }
         });
+
+        // Initialiser le Spinner
+        Spinner languageSpinner = findViewById(R.id.language);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.language_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        gameView.pause();
+    }
+
+    private void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        gameView.resume();
     }
 }
