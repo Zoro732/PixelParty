481b90fc7449495e02d47b2c63d42a65c34e771c
Amaury <amaury.gielen@edu.esiee.fr>
2024-10-31 18:21:46 +0100

    First version prototype board cleaned

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,23 +2,33 @@ package com.example.helloworld;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Build;
-import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FrameLayout gameFrame; // Déclaration de FrameLayout
     private RecyclerView recyclerView;
     private BoardAdapter boardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            hideNavigationBar();
+        }
         setContentView(R.layout.activity_main);
 
+        gameFrame = findViewById(R.id.gameFrame);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -31,5 +41,14 @@ public class MainActivity extends AppCompatActivity {
         // Adapter
         boardAdapter = new BoardAdapter(boardCells);
         recyclerView.setAdapter(boardAdapter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 }
