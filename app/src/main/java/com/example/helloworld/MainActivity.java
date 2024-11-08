78c6401c779395f49df4d6a9750fb60bbbe94940
Amaury <amaury.gielen@edu.esiee.fr>
2024-11-08 17:44:57 +0100

    Third version Taquin : Affichage solution

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,12 +6,12 @@ import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.helloworld.R;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView[][] tiles = new TextView[4][4];
+    private TextView[][] solutionTiles = new TextView[4][4];
     private int emptyRow = 3, emptyCol = 3; // Position initiale de l'espace vide
 
     @Override
@@ -19,83 +19,80 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Cacher la barre de navigation pour le mode plein écran
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION      // Cacher la barre de navigation
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN         // Cacher la barre de statut
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY   // Activer le mode immersif
-        );
+        // Masquer complètement la barre de statut et la barre de navigation
+        hideSystemUI();
 
-        // Récupérer le GridLayout
+        // Récupérer les GridLayouts pour les tuiles du jeu et de la solution
         GridLayout gridLayout = findViewById(R.id.gridLayout);
+        GridLayout solutionGrid = findViewById(R.id.solutionGrid);
 
-        if (gridLayout == null) {
-            // Log si le GridLayout n'est pas trouvé
-            return; // Sortir si la vue n'est pas trouvée
+        // Initialisation des tuiles du jeu
+        initializeTiles(gridLayout, tiles);
+
+        // Initialisation des tuiles de la solution
+        initializeSolutionTiles(solutionGrid);
+
+        // Mélanger les tuiles du jeu
+        shuffleTiles(); // Mélanger les tuiles au démarrage
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI(); // Assurer que le mode plein écran est réactivé lors de la reprise
+    }
+
+    private void hideSystemUI() {
+        // Masquer la barre de statut et la barre de navigation en utilisant les options modernes
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    private void initializeTiles(GridLayout gridLayout, TextView[][] tileArray) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int resID = getResources().getIdentifier("tile_" + (i * 4 + j + 1), "id", getPackageName());
+                if (resID == 0) {
+                    resID = getResources().getIdentifier("tile_empty", "id", getPackageName());
+                }
+                TextView tile = findViewById(resID);
+                tileArray[i][j] = tile;
+
+                tile.setText(String.valueOf(i * 4 + j + 1)); // Remplir les tuiles avec les valeurs
+                if (i == 3 && j == 3) {
+                    tile.setText(""); // La dernière tuile est vide
+                }
+
+                final int row = i;
+                final int col = j;
+                tile.setOnClickListener(v -> onTileClick(row, col));
+            }
         }
+    }
 
-        try {
-            // Initialisation des tuiles avec les IDs
-            TextView tile1 = findViewById(R.id.tile_1);
-            TextView tile2 = findViewById(R.id.tile_2);
-            TextView tile3 = findViewById(R.id.tile_3);
-            TextView tile4 = findViewById(R.id.tile_4);
-            TextView tile5 = findViewById(R.id.tile_5);
-            TextView tile6 = findViewById(R.id.tile_6);
-            TextView tile7 = findViewById(R.id.tile_7);
-            TextView tile8 = findViewById(R.id.tile_8);
-            TextView tile9 = findViewById(R.id.tile_9);
-            TextView tile10 = findViewById(R.id.tile_10);
-            TextView tile11 = findViewById(R.id.tile_11);
-            TextView tile12 = findViewById(R.id.tile_12);
-            TextView tile13 = findViewById(R.id.tile_13);
-            TextView tile14 = findViewById(R.id.tile_14);
-            TextView tile15 = findViewById(R.id.tile_15);
-            TextView tileEmpty = findViewById(R.id.tile_empty);
+    private void initializeSolutionTiles(GridLayout solutionGrid) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int resID = getResources().getIdentifier("tile_" + (i * 4 + j + 1) + "_solution", "id", getPackageName());
+                if (resID == 0) {
+                    resID = getResources().getIdentifier("tile_empty_solution", "id", getPackageName());
+                }
+                TextView tile = findViewById(resID);
+                solutionTiles[i][j] = tile;
 
-            // Mettre les TextViews dans le tableau tiles
-            tiles[0][0] = tile1;
-            tiles[0][1] = tile2;
-            tiles[0][2] = tile3;
-            tiles[0][3] = tile4;
-            tiles[1][0] = tile5;
-            tiles[1][1] = tile6;
-            tiles[1][2] = tile7;
-            tiles[1][3] = tile8;
-            tiles[2][0] = tile9;
-            tiles[2][1] = tile10;
-            tiles[2][2] = tile11;
-            tiles[2][3] = tile12;
-            tiles[3][0] = tile13;
-            tiles[3][1] = tile14;
-            tiles[3][2] = tile15;
-            tiles[3][3] = tileEmpty;
-
-            // Initialiser les tuiles avec les numéros
-            int number = 1;
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (tiles[i][j] != tileEmpty) {
-                        tiles[i][j].setText(String.valueOf(number++));
-                    } else {
-                        tiles[i][j].setText(""); // L'espace vide
-                    }
-                    final int row = i;
-                    final int col = j;
-
-                    // Augmenter la taille des tuiles ici aussi
-                    tiles[i][j].getLayoutParams().width = 120;  // Ajuster la largeur
-                    tiles[i][j].getLayoutParams().height = 120; // Ajuster la hauteur
-
-                    tiles[i][j].setOnClickListener(v -> onTileClick(row, col));
+                // Remplir les tuiles avec les valeurs de la solution
+                tile.setText(String.valueOf(i * 4 + j + 1)); // Remplir les tuiles avec les valeurs
+                if (i == 3 && j == 3) {
+                    tile.setText(""); // La dernière tuile est vide dans la solution
                 }
             }
-
-            shuffleTiles(); // Mélanger les tuiles au démarrage
-
-        } catch (Exception e) {
-            // Gérer les erreurs d'initialisation
         }
     }
 
@@ -135,13 +132,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isGameWon() {
-        int number = 1;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (tiles[i][j] != null && !tiles[i][j].getText().toString().equals(String.valueOf(number))) {
+                if (!tiles[i][j].getText().toString().equals(String.valueOf(i * 4 + j + 1))) {
                     return false;
                 }
-                number++;
             }
         }
         return true;
